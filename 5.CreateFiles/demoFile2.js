c8681b583ec153ade0a53b7a78e2e983e9605e3a
sagar-kumar-yadav <sk5323912@gmail.com>
2023-04-24 03:48:33 +0530

    new commit

--- 5.CreateFiles/demoFile2.js
+++ 5.CreateFiles/demoFile2.js
@@ -4,8 +4,10 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.open('myNewFile2.txt', 'w', function(err, file){
-        if (err) throw err;
-        console.log("Saved");
+    fs.open('deno1.txt', 'w', function(err, file){
+        if (err) {
+            console.log(err);
+        };
+        console.log('saved');
     });
 }).listen(8080);
--- 5.CreateFiles/demoFile3.js
+++ 5.CreateFiles/demoFile3.js
@@ -2,7 +2,7 @@ var http = require('http');
 var fs = require('fs');
 
 http.createServer(function (req, res) {
-    fs.writeFile('myNewFile3.txt', 'Hello content!', function(err){
+    fs.writeFile('myNewFile3.txt', 'hello my name is sagar', function(err){
         if (err) throw err;
         console.log("Saved");
     });
